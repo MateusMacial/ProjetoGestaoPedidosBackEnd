b712a18d19301dcbda0372e08e275780721f1f3d
mateusmacial <mateus_macial@hotmail.com>
2021-12-23 13:09:25 -0300

    first commit

---
+++ src/main/java/com/mateusmacial/projetogestaodepedidos/ProjetogestaodepedidosApplication.java
@@ -0,0 +1,13 @@
+package com.mateusmacial.projetogestaodepedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetogestaodepedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetogestaodepedidosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mateusmacial/projetogestaodepedidos/ProjetogestaodepedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mateusmacial.projetogestaodepedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetogestaodepedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
